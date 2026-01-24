fcb70beced2a22e2089c96895db2f78611323344
Matt Heaphy <matt.heaphy@gmail.com>
2026-01-24 00:30:54 -0500

    reorganized into separate functions

--- R/read_mort_soa.R
+++ R/read_mort_soa.R
@@ -40,30 +40,36 @@
 #'   metadata attributes described above.
 #' @export
 read_mort_soa <- function(table_id) {
+  xml <- check_get_xml(table_id)
+  content_meta <- extract_content_meta(xml)
+  tbls <- xml2::xml_find_all(xml, "Table")
+  res <- purrr::map(tbls, xml_to_df)
+  content_meta$sub_descriptions <- purrr::map_chr(res, \(x) {
+    attr(x, "description")
+  })
+  attributes(res) <- content_meta
+  res
+}
+
+check_get_xml <- function(table_id) {
   resp <- httr2::request("https://mort.soa.org") |>
     httr2::req_url_path_append("Export.aspx") |>
     httr2::req_url_query(Type = "xml", TableIdentity = table_id) |>
     httr2::req_perform()
 
   tryCatch(
-    xml <- resp |> httr2::resp_body_xml(),
+    resp |> httr2::resp_body_xml(),
     error = function(e) {
       cli::cli_abort(
         "Table #{table_id} is not available on {.url https://mort.soa.org}"
       )
     }
   )
+}
 
+extract_content_meta <- function(xml) {
   # Child item 1 = meta data
   content_meta <- xml |> xml2::xml_child(1)
-  get_meta <- \(x, meta) {
-    x |>
-      # purrr::set_names() |>
-      purrr::map(\(y) {
-        xml2::xml_find_all(meta, paste0(".//", y)) |> xml2::xml_text()
-      })
-  }
-
   content_meta <- c(
     name = "TableName",
     table_id = "TableIdentity",
@@ -75,39 +81,40 @@ read_mort_soa <- function(table_id) {
   content_meta$layout <- content_meta$kw[[1]]
   content_meta$nation <- content_meta$kw[[length(content_meta$kw)]]
   content_meta$kw <- NULL
+  content_meta
+}
 
-  xml_to_df <- function(xml) {
-    # Table child item 1 = meta data
-    meta <- xml2::xml_child(xml, 1)
-    # Get axis definitions
-    axis_def <- xml2::xml_find_all(meta, "AxisDef") |> xml2::as_list()
-    axes <- list()
-    for (x in axis_def) {
-      axes[[x$AxisName[[1]]]] <- seq.int(
-        as.numeric(x$MinScaleValue[[1]]),
-        as.numeric(x$MaxScaleValue[[1]]),
-        by = as.numeric(x$Increment[[1]])
-      )
-    }
+get_meta <- \(x, meta) {
+  x |>
+    # purrr::set_names() |>
+    purrr::map(\(y) {
+      xml2::xml_find_all(meta, paste0(".//", y)) |> xml2::xml_text()
+    })
+}
 
-    qx <- xml2::xml_child(xml, 2) |>
-      xml2::xml_find_all(".//Y") |>
-      xml2::xml_double()
-
-    dat <- do.call(tidyr::expand_grid, rev(axes)) |>
-      dplyr::as_tibble() |>
-      dplyr::mutate(qx = qx)
-
-    attr(dat, "description") <- get_meta("TableDescription", meta)[[1]]
-
-    dat
+xml_to_df <- function(xml) {
+  # Table child item 1 = meta data
+  meta <- xml2::xml_child(xml, 1)
+  # Get axis definitions
+  axis_def <- xml2::xml_find_all(meta, "AxisDef") |> xml2::as_list()
+  axes <- list()
+  for (x in axis_def) {
+    axes[[x$AxisName[[1]]]] <- seq.int(
+      as.numeric(x$MinScaleValue[[1]]),
+      as.numeric(x$MaxScaleValue[[1]]),
+      by = as.numeric(x$Increment[[1]])
+    )
   }
 
-  tbls <- xml2::xml_find_all(xml, "Table")
-  res <- purrr::map(tbls, xml_to_df)
-  content_meta$sub_descriptions <- purrr::map_chr(res, \(x) {
-    attr(x, "description")
-  })
-  attributes(res) <- content_meta
-  res
+  qx <- xml2::xml_child(xml, 2) |>
+    xml2::xml_find_all(".//Y") |>
+    xml2::xml_double()
+
+  dat <- do.call(tidyr::expand_grid, rev(axes)) |>
+    dplyr::as_tibble() |>
+    dplyr::mutate(qx = qx)
+
+  attr(dat, "description") <- get_meta("TableDescription", meta)[[1]]
+
+  dat
 }
